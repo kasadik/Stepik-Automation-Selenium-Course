937fb2b88359037894936b09a07c1e00222492ab
Bohdan Krutskevych <lyskav>
2021-10-25 12:50:14 +0300

    add simple test case

---
+++ lesson_1_6.py
@@ -0,0 +1,21 @@
+import math
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+
+
+try:
+
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+    text = WebDriverWait(browser,12).until(EC.text_to_be_present_in_element((By.ID,'price'),'$100'))
+    browser.find_element(By.ID,'book').click()
+    x=int(browser.find_element(By.ID,'input_value').text)
+    solution=math.log(abs(12*math.sin(x)))
+    browser.find_element(By.ID,'answer').send_keys(str(solution))
+    browser.find_element(By.ID,'solve').click()
+finally:
+    time.sleep(5)
+    browser.quit()

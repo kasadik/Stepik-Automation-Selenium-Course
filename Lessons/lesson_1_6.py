ce754038a37cd7732a8fed47965267f15fe8e614
Bohdan Krutskevych <lyskav>
2021-10-26 13:43:02 +0300

    Added unittest to lesson

--- Lessons/lesson_1_6.py
+++ Lessons/lesson_1_6.py
@@ -4,18 +4,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
+import unittest
 
-
-try:
-
-    browser = webdriver.Chrome()
-    browser.get("http://suninjuly.github.io/explicit_wait2.html")
-    text = WebDriverWait(browser,12).until(EC.text_to_be_present_in_element((By.ID,'price'),'$100'))
-    browser.find_element(By.ID,'book').click()
-    x=int(browser.find_element(By.ID,'input_value').text)
-    solution=math.log(abs(12*math.sin(x)))
-    browser.find_element(By.ID,'answer').send_keys(str(solution))
-    browser.find_element(By.ID,'solve').click()
-finally:
-    time.sleep(5)
-    browser.quit()
+class TestWeb(unittest.TestCase):
+    def test_page(self):
+        try:
+            browser = webdriver.Chrome()
+            browser.get("http://suninjuly.github.io/explicit_wait2.html")
+            text = WebDriverWait(browser,12).until(EC.text_to_be_present_in_element((By.ID,'price'),'$100'))
+            browser.find_element(By.ID,'book').click()
+            x=int(browser.find_element(By.ID,'input_value').text)
+            solution=math.log(abs(12*math.sin(x)))
+            browser.find_element(By.ID,'answer').send_keys(str(solution))
+            browser.find_element(By.ID,'solve').click()
+        finally:
+            time.sleep(5)
+            browser.quit()

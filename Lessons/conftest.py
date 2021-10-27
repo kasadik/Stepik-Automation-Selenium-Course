302577f670f8b41ca0acce739e1fbd8ff038807c
Bohdan Krutskevych <lyskav>
2021-10-27 18:18:33 +0300

    Added browser choosing functionality

--- Lessons/conftest.py
+++ Lessons/conftest.py
@@ -1,10 +1,26 @@
 import pytest
 from selenium import webdriver
 import time
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+
 @pytest.fixture(scope="function")
-def browser():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(3)
-    yield browser
+def browser(request):
+    browser_name=request.config.getoption('browser_name')
+    if browser_name=='chrome':
+        browser=webdriver.Chrome()
+        browser.implicitly_wait(3)
+        yield browser
+        browser.quit()
+    if browser_name=='firefox':
+        browser=webdriver.Firefox()
+        browser.implicitly_wait(3)
+        yield browser
+        browser.quit()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+
     time.sleep(1)
     browser.quit()

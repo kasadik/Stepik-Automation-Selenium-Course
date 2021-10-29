b00c4e11a48454cf5c48cb726d2f13afd41eaba3
Bohdan <lyskav@gmail.com>
2021-10-29 19:17:50 +0300

    fixed test's name

--- Lessons/conftest.py
+++ Lessons/conftest.py
@@ -1,4 +1,3 @@
-import time
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -14,6 +13,7 @@ def browser(request):
     browser_name=request.config.getoption('browser_name')
     user_language=request.config.getoption('language')
     options=Options()
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     if browser_name=='chrome':
         browser=webdriver.Chrome(options=options)
--- Lessons/test_items.py
+++ Lessons/test_items.py
@@ -2,8 +2,8 @@ import time
 from selenium.webdriver.common.by import By
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_items(browser):
+def test_add_to_cart_button(browser):
     browser.get(link)
-    time.sleep(30)
+    time.sleep(3)
     text=browser.find_element(By.CSS_SELECTOR,'.btn.btn-lg.btn-primary.btn-add-to-basket').text
     assert  "Ajouter au panier"==text

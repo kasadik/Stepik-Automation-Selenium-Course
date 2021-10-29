095769dd62ca3fdfaba6b9a16a2145faad41d15f
Bohdan <lyskav@gmail.com>
2021-10-29 19:32:21 +0300

    fixed test assertion

--- Lessons/test_items.py
+++ Lessons/test_items.py
@@ -5,5 +5,5 @@ link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_add_to_cart_button(browser):
     browser.get(link)
     time.sleep(3)
-    text=browser.find_element(By.CSS_SELECTOR,'.btn.btn-lg.btn-primary.btn-add-to-basket').text
-    assert  "Ajouter au panier"==text
+    button=browser.find_element(By.CSS_SELECTOR,'.btn.btn-lg.btn-primary.btn-add-to-basket')
+    assert  button
